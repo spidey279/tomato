d24e94d25ae9a24672dab87ca1fcfcb2083a0310
yyyaaassshh <yashpawar2995@gmail.com>
2025-07-05 21:47:21 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[#FFFEFA]">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { TiThMenu } from "react-icons/ti";
 import { ImCross } from "react-icons/im";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   const [navMenuToggle, setnavMenuToggle] = useState(false);
@@ -13,25 +14,45 @@ const Navbar = () => {
           <div className="w-20">
             <img src={logo} className="w-full h-auto" alt="" />
           </div>
-          <TiThMenu
-            onClick={() => setnavMenuToggle(!navMenuToggle)}
-            className={`${navMenuToggle ? " hidden" : "z-10000 block"} text-xl`}
-          />
-          <ImCross
-            onClick={() => setnavMenuToggle(!navMenuToggle)}
-            className={`${navMenuToggle ? "z-10000 block" : "hidden"} text-xl`}
-          />
+          <div className="flex items-center gap-3">
+            <FaShoppingCart className="text-2xl" />
+
+            <TiThMenu
+              onClick={() => setnavMenuToggle(!navMenuToggle)}
+              className={`${
+                navMenuToggle ? " hidden" : " z-10000 block"
+              } text-xl`}
+            />
+            <ImCross
+              onClick={() => setnavMenuToggle(!navMenuToggle)}
+              className={`${
+                navMenuToggle ? "z-10000 block" : "hidden"
+              } text-xl`}
+            />
+          </div>
         </nav>
       </div>
       <div
         className={`${
           navMenuToggle ? "translate-x-0   " : "translate-x-[110%]  "
-        }  duration-[600ms] top-0 z-50 fixed   ease-in-out  h-screen w-full bg-red-400`}
+        }  duration-[600ms] top-0 z-50 fixed text-black  ease-in-out  h-screen w-full bg-[#FFFEFA]`}
       >
-        <ul>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
+        <ul className="flex flex-col items-center gap-10 font-semibold  mt-15">
+          <li className="bg-orange-500 p-1 w-30 text-center rounded-md">
+            home{" "}
+          </li>
+          <li className="bg-orange-500 p-1 w-30 text-center rounded-md">
+            menu
+          </li>
+          <li className="bg-orange-500 p-1 w-30 text-center rounded-md">
+            mobile-app
+          </li>
+          <li className="bg-orange-500 p-1 w-30 text-center rounded-md">
+            contact us
+          </li>
+          <li className="bg-white border-orange-500 border-2 p-1 w-30 text-center rounded-md">
+            Sign In
+          </li>
         </ul>
       </div>
     </div>
